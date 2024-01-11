e49a7ef6b3b2acf4d0a6410e236dff79f48e560f
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-01-11 21:34:44 +0100

    v2 completed!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [peopleInSpace, setPeopleInSpace] = useState([]);
+  const [activeCraft, setActiveCraft] = useState("All");
+
+  useEffect(() => {
+    async function fetchPeopleInSpace() {
+      const response = await fetch("http://api.open-notify.org/astros.json");
+      const data = await response.json();
+      setPeopleInSpace(data.people);
+    }
+    fetchPeopleInSpace();
+  }, []);
+
+  const handleCraftChange = (event) => {
+    setActiveCraft(event.target.value);
+  };
+
+  const filteredPeople =
+    activeCraft === "All"
+      ? peopleInSpace
+      : peopleInSpace.filter((person) => person.craft === activeCraft);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <main>
+        <h1 className="firstSentence">
+          There {filteredPeople.length === 1 ? "is" : "are"} currently{" "}
+          <span className="dynamic">{filteredPeople.length}</span> people{" "}
+          {activeCraft === "All" ? "in" : "on the"}
+        </h1>
+        <select value={activeCraft} onChange={handleCraftChange}>
+          <option value="All">space</option>
+          <option value="ISS">ISS</option>
+          <option value="Tiangong">Tiangong</option>
+        </select>
+        {filteredPeople.length > 0 && (
+          <>
+            <h1>Their names are: </h1>
+            <h1 className="dynamic">
+              {filteredPeople.map((person, index) => (
+                <span key={person.name}>
+                  {person.name}
+                  {index < filteredPeople.length - 1 && ", "}
+                </span>
+              ))}
+            </h1>
+          </>
+        )}
+      </main>
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Made in REACT with love and thanks to{" "}
+          <a href="http://open-notify.org/Open-Notify-API/People-In-Space/">
+            Open Notify API
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </footer>
+    </>
   );
 }
 
